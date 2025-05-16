41db506c8d364b05bcbe97cc3f3d02d722ba0249
AnshumaanMishra <anshumaanmishra.24@kgpian.iitkgp.ac.in>
2025-05-16 16:35:33 +0530

    Added 30 day local storage for Task Lists

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Cookies from 'js-cookie';
 
 function inList(list, item) {
     for (var i = 0; i < list.length; i++) {
@@ -33,7 +34,52 @@ function ToDo() {
     const [incomplete, setInComplete] = useState([]);
     const [complete, setComplete] = useState([]);
 
+    const [loaded, setLoaded] = useState(false);
+    
+  useEffect(() => {
+    const loadCookieData = (name, setter) => {
+        const cookieValue = Cookies.get(name);
+
+        if (cookieValue) {
+            try {
+            const parsed = JSON.parse(cookieValue);
+            if (Array.isArray(parsed)) {
+                setter(parsed);
+            } else {
+                console.warn(`${name} cookie is not an array, resetting...`);
+                Cookies.set(name, JSON.stringify([]), { expires: 7 });
+                setter([]);
+            }
+            } catch (e) {
+            console.error(`Failed to parse ${name} cookie:`, e);
+            Cookies.set(name, JSON.stringify([]), { expires: 30});
+            setter([]);
+            }
+        } else {
+            Cookies.set(name, JSON.stringify([]), { expires: 30});
+            setter([]);
+        }
+    };
+
+    loadCookieData('complete', setComplete);
+    loadCookieData('incomplete', setInComplete);
+
+    setLoaded(true);
+    }, []);
     var buffer;
+
+    useEffect(() => {
+        if (loaded) {
+            Cookies.set('complete', JSON.stringify(complete), { expires: 30 });
+        }
+    }, [complete, loaded]);
+
+    useEffect(() => {
+        if (loaded) {
+            Cookies.set('incomplete', JSON.stringify(incomplete), { expires: 30 });
+        }
+    }, [incomplete, loaded]);
+
 
     function bufferElement(event) {
         buffer = event.currentTarget.innerText;
@@ -119,7 +165,9 @@ function ToDo() {
             <h1>Simple To-Do App</h1>
             <div className="text-box">
                 <input type="text" placeholder='Enter task' onKeyDown={keyLod} id='task-getter' />
-                <button onClick={addTask}>Add task</button>
+                <button onClick={addTask}>
+                    +
+                </button>
             </div>
             <div className="tasks-view">
                 <div className="incomplete"
@@ -136,6 +184,7 @@ function ToDo() {
                             incomplete.map(todo => {
                                 return (
                                     <li
+                                        key={todo}
                                         className='list-item'
                                         draggable="true"
                                         onDragStart={(event) => bufferElement(event)}
@@ -177,6 +226,7 @@ function ToDo() {
                         {complete.map(todo => {
                             return (
                                 <li
+                                    key={todo}
                                     className='list-item'
                                     draggable="true"
                                     onDragStart={(event) => bufferElement(event)}
